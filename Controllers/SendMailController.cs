054ecd50ec39a6dc99b12cc9df49dc974d026138
Ahmedbinfaisal <67901435+AhmedFaisal11@users.noreply.github.com>
2021-09-23 15:34:02 +0500

    Add files via upload

---
+++ Controllers/SendMailController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendMailThroughMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SendMailThroughMVC.Controllers
+{
+    public class SendMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SendMailModel _objectModel)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage message = new MailMessage();
+                message.To.Add(_objectModel.To);
+                message.From = new MailAddress(_objectModel.From);
+                message.Subject = _objectModel.Subject;
+                string Body = _objectModel.Body;
+                message.Body = Body;
+                message.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("Username", "Password"); // Enter seders User name and password       
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SendMailThroughMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
